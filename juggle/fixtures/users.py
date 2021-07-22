2568c9e938303a484ddc8b1dea8494802815e01c
Mateusz Kieblesz <mattkieblesz@hotmail.com>
2021-07-22 21:42:42 +0200

    Run black

--- juggle/fixtures/users.py
+++ juggle/fixtures/users.py
@@ -1,22 +1,22 @@
 from django.contrib.auth.models import User
 
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser(
-        username='admin',
-        email='admin@example.com',
-        password='admin',
-        first_name='Mr',
-        last_name='Admin',
+        username="admin",
+        email="admin@example.com",
+        password="admin",
+        first_name="Mr",
+        last_name="Admin",
     )
 
 User.objects.get_or_create(
-    username='user1',
-    first_name='John',
-    last_name='Smith',
+    username="user1",
+    first_name="John",
+    last_name="Smith",
 )
 
 User.objects.get_or_create(
-    username='user2',
-    first_name='Mark',
-    last_name='Twain',
+    username="user2",
+    first_name="Mark",
+    last_name="Twain",
 )
--- juggle/settings.py
+++ juggle/settings.py
@@ -8,84 +8,84 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str(
-    'SECRET_KEY', default='django-insecure-e-5!y!2*ksu^f^(q4w5hh!b%b!8f3vlrc6h2^sq@ozz)dl8z%p'
+    "SECRET_KEY",
+    default="django-insecure-e-5!y!2*ksu^f^(q4w5hh!b%b!8f3vlrc6h2^sq@ozz)dl8z%p",
 )
-DEBUG = env.bool('API_DEBUG', default=True)
+DEBUG = env.bool("API_DEBUG", default=True)
 
-ALLOWED_HOSTS: List[str] = env.list('ALLOWED_HOSTS', default=['*'])
+ALLOWED_HOSTS: List[str] = env.list("ALLOWED_HOSTS", default=["*"])
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'django_probes',
-
-    'juggle',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_probes",
+    "rest_framework",
+    "juggle",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'juggle.urls'
+ROOT_URLCONF = "juggle.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'juggle.wsgi.application'
+WSGI_APPLICATION = "juggle.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('POSTGRES_DB', default='juggle'),
-        'USER': env.str('POSTGRES_USER', default='juggle'),
-        'PASSWORD': env.str('POSTGRES_PASSWORD', default='juggle'),
-        'HOST': env.str('POSTGRES_HOST', default='0.0.0.0'),
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("POSTGRES_DB", default="juggle"),
+        "USER": env.str("POSTGRES_USER", default="juggle"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD", default="juggle"),
+        "HOST": env.str("POSTGRES_HOST", default="0.0.0.0"),
+        "PORT": 5432,
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -93,6 +93,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
