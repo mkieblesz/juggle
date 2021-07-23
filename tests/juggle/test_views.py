21e43a75e84fcfa049105143e05991e122937595
Mateusz Kieblesz <mattkieblesz@hotmail.com>
2021-07-23 08:54:30 +0200

    Added tests todo

--- tests/juggle/test_views.py
+++ tests/juggle/test_views.py
@@ -1,2 +1,18 @@
-def test_example():
-    assert 2 + 2 == 4
+def test_list_all_entities_listed_on_the_website():
+    pass
+
+
+def test_search_for_entities():
+    pass
+
+
+def test_allow_to_list_all_applicants_for_any_job():
+    pass
+
+
+def test_allow_professional_to_apply_for_any_job():
+    pass
+
+
+def test_limit_to_5_applications_per_job_per_day():
+    pass
