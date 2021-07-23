b955c3d6e3ef6010fc41fec7d47ef57ba5ca2080
Mateusz Kieblesz <mattkieblesz@hotmail.com>
2021-07-23 12:05:28 +0200

    Update

--- juggle/views.py
+++ juggle/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from datetime import date
+
 
 from juggle.models import Business, Job, JobApplication, Professional
 from juggle.serializers import JobApplicationSerializer, JobSerializer
@@ -33,6 +35,9 @@ class JobApplicationViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        if JobApplication.objects.filter(serializer.job, date=date.today()).count() >= 5:
+            return Response({"message": "You can't apply for this job today"}, status=400)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=200, headers=headers)
